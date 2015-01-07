c5e010298ffb7bc7debbc879fedace3fef00dba6
Lucas Arnström <lucasarnstrom@gmail.com>
2015-01-07 15:28:19 +0100

    tjosan

--- src/main/java/ioopm/pong/model/MyPongModel.java
+++ src/main/java/ioopm/pong/model/MyPongModel.java
@@ -5,34 +5,44 @@ import java.util.Set;
 
 public class MyPongModel implements PongModel {
 
-	private final static Dimension FIELD_SIZE = new Dimension(1000, 2000);
+	private final static Dimension FIELD_SIZE = new Dimension(2000, 1000);
 
 	private final String LEFT_PLAYERNAME;
 	private final String RIGHT_PLAYERNAME;
-	
-	private int left_barpos = 0;
-	private int right_barpos = 0;
 
+	private int left_barheight  = 100;
+	private int right_barheight = 100;
+	
+	private int left_barpos  = left_barheight / 2;
+	private int right_barpos = right_barheight / 2;
+	
+	private final int BALL_SPEED = 3;
+	
 	private final Point  ball           = new Point((int) FIELD_SIZE.getWidth() / 2, (int) (FIELD_SIZE.getHeight() / 2));
-	private final Vector ball_direction = new Vector(Math.random(), Math.random());
+	private final Vector ball_direction = new Vector(Math.random() * BALL_SPEED - BALL_SPEED / 2, Math.random() * BALL_SPEED - BALL_SPEED / 2);
 
 	public MyPongModel(String leftPlayer, String rightPlayer) {
 		this.LEFT_PLAYERNAME = leftPlayer;
 		this.RIGHT_PLAYERNAME = rightPlayer;
+		
+		ball_direction.normalize();
+		//TODO multiply ball_direction with ball speed
 	}
 
 	@Override
 	public void compute(Set<Input> input, long delta_t) {
+		
+		//TODO restrict movement of bars
 		for(Input in : input) {
 			switch(in.key) {
 				case LEFT:
 					switch(in.dir) {
 						case UP:
-							this.left_barpos -= 1;
+							this.left_barpos -= 10;
 							break;
 						
 						case DOWN:
-							this.left_barpos += 1;
+							this.left_barpos += 10;
 							break;
 					}
 					break;
@@ -40,18 +50,25 @@ public class MyPongModel implements PongModel {
 				case RIGHT:
 					switch(in.dir) {
 						case UP:
-							this.right_barpos -= 1;
+							this.right_barpos -= 10;
 							break;
 						
 						case DOWN:
-							this.right_barpos += 1;
+							this.right_barpos += 10;
 							break;
 					}
 					break;
 			}
 		}
 		
-		//TODO move ball.
+		//TODO bounce ball
+		
+		if(ball.getX() <= 0) {
+			// Hit left wall
+
+		}
+		
+		ball.setLocation(ball.getX() + ball_direction.getX(), ball.getY() + ball_direction.getY());
 	}
 
 	@Override
@@ -63,13 +80,21 @@ public class MyPongModel implements PongModel {
 				return this.right_barpos;
 		}
 		
-		// This can never be reached.
+		// This dead code.
 		return 0;
 	}
 
 	@Override
 	public int getBarHeight(BarKey k) {
-		return 20;
+		switch(k) {
+			case LEFT:
+				return left_barheight;
+			case RIGHT:
+				return right_barheight;
+		}
+		
+		// This is dead code.
+		return 0;
 	}
 
 	@Override
@@ -86,7 +111,7 @@ public class MyPongModel implements PongModel {
 	@Override
 	public String getScore(BarKey k) {
 		//TODO fix getScore()
-		return "0";
+		return "2";
 	}
 
 	@Override
